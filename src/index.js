b4b3e5bcee751e767ab54568b8e70fa85a1b6102
HoanDo <hoan.yesi1@gmail.com>
2018-08-26 11:50:50 +0700

    initial skeleton

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import App from "./components/app";
+import reducers from "./reducers";
+
+const createStoreWithMiddleWare = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleWare(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector(".container")
+);
